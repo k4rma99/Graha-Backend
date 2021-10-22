edc40b88135c619c5b6625eefc90794511bc5f36
k4rma <k4.rma321@gmail.com>
2021-10-22 09:33:45 +0530

    New Cloudinary Added

--- config/plugins.js
+++ config/plugins.js
@@ -16,9 +16,9 @@ module.exports = ({ env }) => ({
 //     upload: {
 //       provider: 'cloudinary',
 //       providerOptions: {
-//         cloud_name: "grahastudio",
-//         api_key: "239144481548437",
-//         api_secret: "V_5xIAfOc8li3lxZ1OOIwdbBOrg",
+//         cloud_name: "graha",
+//         api_key: "131425146859753",
+//         api_secret: "1hpgLOqm_77WHBrCB9-ktRoR7UU",
 //       },
 //     },
 //     // ...
