afd70db30eedc1e705d1aa64f74ff461165df369
Vinay <52606733+k4rma99@users.noreply.github.com>
2021-11-05 17:23:52 +0530

    Upload problems

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: 'graha',
+        api_key: '131425146859753',
+        api_secret: '1hpgLOqm_77WHBrCB9-ktRoR7UU',
       },
     },
     // ...
