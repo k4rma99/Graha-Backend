40b13429ea7de37480f4088f30a21e545a58ca50
Vinay <52606733+k4rma99@users.noreply.github.com>
2022-12-02 20:43:18 +0530

    Remove Netlify

--- config/server.js
+++ config/server.js
@@ -7,12 +7,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-module.exports = ({ env }) => ({
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: env('NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN', 'ZoYFIJOTQADqnX-1P8ocvFseWpasz6Emjc8ZhIKWkRU')
-    },
-  },
-});
